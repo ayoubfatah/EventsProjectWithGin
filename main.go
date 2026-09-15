38759f24bbd0afff1a9017b843a78a9cc7c06c5b
ayoubfatah <ayoubfatah222@gmail.com>
2026-09-15 19:20:05 +0100

    Fix CORS middleware order and clean up main.go

--- main.go
+++ main.go
@@ -9,34 +9,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
 func main(){
 	db.InitDB()
-	server :=	gin.Default()
+	server := gin.Default()
+
 
 	config := cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://eventify-fawn.vercel.app"},
+    	AllowOrigins:     []string{"http://localhost:3000", "https://eventify-fawn.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 	}
+	server.Use(cors.New(config))
+
 
 	server.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Backend is running!"})
 	})
 
-	server.Use(cors.New(config))
-	// models.SeedEvents()
-	
-
-
 
 	routes.RegisterRoutes(server)
+
+	// Get PORT from env or use default
 	port := os.Getenv("PORT")
 	if port == "" {
-    port = "8080"
-}
+		port = "8080"
+	}
+
 	server.Run(":" + port)
 }
-
